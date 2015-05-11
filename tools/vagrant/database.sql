04d99e926fd96dba7733e3f9feb9b574abdb4392
Jonas Metzener <jonasmetzener@gmail.com>
2015-05-11 20:30:05 +0200

    Removed vagrant stuff

--- tools/vagrant/database.sql
+++
@@ -1,7 +0,0 @@
--- Create database (drop it if it's already there)
-DROP DATABASE IF EXISTS timetable;
-CREATE DATABASE timetable;
-
--- Create user (drop it if it's already there)
-GRANT ALL ON timetable.* TO 'timetable'@'localhost' IDENTIFIED BY '123qwe';
-FLUSH PRIVILEGES;
