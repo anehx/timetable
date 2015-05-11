6c44b87f5f45fb6e87e43d62cef077573d23666f
jaeiser <fabian.jaeiser@bluewin.ch>
2015-05-11 09:23:56 +0200

    Added table lection

--- timetable.sql
+++ timetable.sql
@@ -16,4 +16,14 @@ CREATE TABLE `course` (
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE  `lection` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `start_time` datetime,
+  `end_time` datetime,
+  `course_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 ALTER TABLE `course` ADD CONSTRAINT `course_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `lection` ADD CONSTRAINT `lection_course` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
