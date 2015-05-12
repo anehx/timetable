ed75ec18b5ce41de3869e3e3996d180a4898bbf7
jaeiser <fabian.jaeiser@bluewin.ch>
2015-05-12 14:31:02 +0200

    Added frontend user validation

--- public/js/user_edit.js
+++ public/js/user_edit.js
@@ -1,33 +1,77 @@
-$(document).ready(function {
+jQuery(function($, document) {
+	'use strict'
 
-	function validation() {
-		var ok = true;
-		var errors = [];
-		var username = document.getElementsByName('username');
-		var first_name = document.getElementsByName('first_name');
-		var last_name = document.getElementsByName('last_name');
-		var new_password = document.getElementsByName('new_password');
-		var confirm_password = document.getElementsByName('confirm_password');
+	var $username = $('input[name="username"]')
+	var $firstName = $('input[name="firstName"]')
+	var $lastName = $('input[name="lastName"]')
+	var $password = $('input[name="password"]')
+	var $confirmPassword = $('input[name="confirmPassword"]')
+	var $submit = $('input[type="submit"]')
 
-		if (trim(username.value) == "" || trim(first_name.value) == "" || trim(last_name.value) == "") {
-			$(username).style('border', '1px solid red');
-			ok = false;
+	var validUsername = false
+	var validFirstName = false
+	var validLastName = false
+	var validPassword = false
+	var validConfirmPassword = false
+
+	$username.on('change input blur', function(e) {
+		validateUsername()
+		toggleSubmitButton()
+	})
+
+	$firstName.on('change input blur', function(e) {
+		validateFirstName()
+		toggleSubmitButton()
+	})
+	
+	$lastName.on('change input blur', function(e) {
+		validateLastName()
+		toggleSubmitButton()
+	})
+
+	$password.on('change input blur', function(e) {
+		validatePassword()
+		toggleSubmitButton()
+	})
+
+	$confirmPassword.on('change input blur', function(e) {
+		validateConfirmPassword()
+		toggleSubmitButton()
+	})	
+
+	$(document).keypress(function(e) {
+		if (e.which == 13 && !(validUsername && validFirstName && validLastName && validPassword && validConfirmPassword)) {
+			e.preventDefault()
 		}
-		if (new_password.value.lenght < 6) {
-			$(new_password).style('border', '1px solid red');
-			ok = false;
-			new_password.focus();
-		}
-		if (new_password.value != confirm_password.value) {
-			$(confirm_password).style('border', '1px solid red');
-			ok = false;
-			confirm_password.focus();
-		}
-		if (!isAlphanumeric(username.value)) {
-			$(username).style('border', '1px solid red;');
-			ok = false;
-			username.focus();
-		}
-		return ok;
+	})
+
+	function toggleSubmitButton() {
+		$submit.toggleClass('disabled', !(validUsername && validFirstName && validLastName && validPassword && validConfirmPassword))
 	}
-});
+
+	function validateUsername() {
+		validUsername = !/[^A-Za-z0-9\s]+/.test($username.val()) && $username.val().length < 50 && $username.val().length > 1
+		$username.parent().toggleClass('has-error', !validUsername)
+	}
+
+	function validateFirstName() {
+		validFirstName = !/[^A-Za-zäöüéàè\s]+/.test($firstName.val()) && $firstName.val().length < 50
+		$firstName.parent().toggleClass('has-error', !validFirstName)	
+	}
+
+	function validateLastName() {
+		validLastName = !/[^A-Za-zäöüéàè\s]+/.test($lastName.val()) && $lastName.val().length < 50
+		$lastName.parent().toggleClass('has-error', !validLastName)
+	}
+
+	function validatePassword() {
+		validPassword = !/[^A-Za-z0-9#%&\/()?!$-_]+/.test($password.val()) && $password.val().length >= 6
+		$password.parent().toggleClass('has-error', !validPassword)
+	}
+
+	function validateConfirmPassword() {
+		validConfirmPassword = !/[^A-Za-z0-9#%&\/()?!$-_]+/.test($confirmPassword.val()) && $confirmPassword.val().length >= 6 && $confirmPassword.val() === $password.val()
+		$confirmPassword.parent().toggleClass('has-error', !validConfirmPassword)
+	}
+})
+
