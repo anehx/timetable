cb5e0ff9e88eab2fee89d2ce7c83dd1707413f35
Jonas Metzener <jonasmetzener@gmail.com>
2015-05-18 16:26:14 +0200

    Removed useless comment

--- app/controller/LessonTimeController.class.php
+++ app/controller/LessonTimeController.class.php
@@ -24,7 +24,7 @@ class LessonTimeController extends Controller {
 					$this->handleDelete();
 					break;
 				default:
-					//$this->handleDefault();
+					$this->handleDefault();
 					break;
 			}
 		}
