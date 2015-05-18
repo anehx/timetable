cb851ec05005ac7afe698a254b0883723b56bdb3
jaeiser <fabian.jaeiser@bluewin.ch>
2015-05-18 16:35:16 +0200

    Added course overview

--- app/controller/BaseController.class.php
+++ app/controller/BaseController.class.php
@@ -1,10 +1,19 @@
 <?php
 
 require_once('controller/Controller.class.php');
+require_once('datamapper/CourseMapper.class.php');
+require_once('datamapper/UserMapper.class.php');
 
 class BaseController extends Controller {
 	/*
 	 The default template
 	*/
 	protected $tpl = 'home.tpl';
+
+	public function handle() {
+		$this->smarty->assign('courses', CourseMapper::getInstance()->getCourses());
+
+		parent::handle();
+	}
+
 }
--- app/templates/home.tpl
+++ app/templates/home.tpl
@@ -1,9 +1,15 @@
 {extends 'index.tpl'}
 
 {block 'content'}
-	<div id="time"></div>
-{/block}
-
-{block 'scripts'}
-	<script src="/js/time.js"></script>
+	{if !empty($courses)}
+	<div class="row">
+		{foreach $courses as $course}
+		<div class="col col-xs-12 col-sm-4">
+			<div class="col-inner">
+				<a href="" title="course">{$course->name}</a>
+			</div>
+		</div>
+		{/foreach}
+	</div>
+	{/if}
 {/block}
