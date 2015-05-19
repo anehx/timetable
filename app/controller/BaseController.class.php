9f3ea2d7456df3235f9803fa2868c248e34d431b
jaeiser <fabian.jaeiser@bluewin.ch>
2015-05-19 11:38:19 +0200

    Added overview

--- app/controller/BaseController.class.php
+++ app/controller/BaseController.class.php
@@ -3,6 +3,7 @@
 require_once('controller/Controller.class.php');
 require_once('datamapper/CourseMapper.class.php');
 require_once('datamapper/UserMapper.class.php');
+require_once('datamapper/LessonTimeMapper.class.php');
 
 class BaseController extends Controller {
 	/*
@@ -10,10 +11,45 @@ class BaseController extends Controller {
 	*/
 	protected $tpl = 'home.tpl';
 
+	/*
+	 Handles all requests on this page
+	*/
 	public function handle() {
-		$this->smarty->assign('courses', CourseMapper::getInstance()->getCourses());
+
+		if (isset($_GET['action'])) {
+			switch ($_GET['action']) {
+				case 'overview':
+					$this->handleOverview();
+					break;
+				default:
+					$this->handleDefault();
+					break;
+			}
+		}
+		else {
+			$this->handleDefault();
+		}
 
 		parent::handle();
 	}
 
+	/*
+	 The default page
+	*/
+	private function handleDefault() {
+		$this->smarty->assign('courses', CourseMapper::getInstance()->getCourses());
+	}
+
+	/*
+	 The lessons
+	*/
+	private function handleOverview() {
+		$this->tpl = 'list.tpl';
+
+		if (isset($_GET['id'])) {
+			$course = CourseMapper::getInstance()->getCourseByID($_GET['id']);
+			$this->smarty->assign('course', $course);
+			$this->smarty->assign('lessonTimes', LessonTimeMapper::getInstance()->getLessonTimes());
+		}
+	}
 }
--- app/templates/home.tpl
+++ app/templates/home.tpl
@@ -5,9 +5,12 @@
 	<div class="row">
 		{foreach $courses as $course}
 		<div class="col col-xs-12 col-sm-4">
+		<a href="/?action=overview&amp;id={$course->id}" title="lessons">
 			<div class="col-inner">
-				<a href="" title="course">{$course->name}</a>
+				<h2>{$course->name}</h2>
+				<i>{if $course->userID}{$course->getUser()->getDisplayName()}{else}{/if}</i>
 			</div>
+		</a>
 		</div>
 		{/foreach}
 	</div>
