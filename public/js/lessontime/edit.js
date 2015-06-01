776be5d3d09a0ff5215192d6d7e5f2276bae3836
Jonas Metzener <jonasmetzener@gmail.com>
2015-06-01 13:32:24 +0200

    Added js doc

--- public/js/lessontime/edit.js
+++ public/js/lessontime/edit.js
@@ -23,8 +23,25 @@ jQuery(function() {
 	,	{
 			selector: 'input[name="endTime"]'
 		,	callback: function(selector) {
-				return $(selector).checkTime()
+				return $(selector).checkTime() && compareTimeString(
+						$('input[name="startTime"]').val(),
+						$(selector).val()
+					)
 			}
 		}
 	])
+
+	/**
+	 * Checks if the "to" time is later than the "from" time
+	 */
+	function compareTimeString(from, to) {
+		var dtFrom = new Date()
+		var dtTo   = new Date()
+
+		// parse time string to date object
+		dtFrom.setHours(from.split(':')[0], from.split(':')[1], 0, 0)
+		dtTo.setHours(to.split(':')[0], to.split(':')[1], 0, 0)
+
+		return dtTo > dtFrom;
+	}
 })
--- public/js/validator.js
+++ public/js/validator.js
@@ -1,5 +1,26 @@
 'use strict'
 
+/**
+ *
+ * Basic validator class
+ *
+ * Use addListener to listen to a form field
+ * and block buttons and shortkeys if field
+ * is not valid
+ *
+ * Example:
+ * 
+ * var validator = new Validator()
+ * validator.init([
+ *	 {
+ *     selector: 'input[name="username"]'
+ *   , callback: function(selector) {
+ *	     return $(selector).checkLength(1, 50)
+ *     }
+ *   }  
+ * ])
+ *
+ */
 function Validator($submit) {
 	this.$submit = $submit
 	this.fields  = {}
@@ -48,26 +69,47 @@ Validator.prototype = {
 	}
 }
 
+/**
+ * Checks if a string contains special chars (mutations doesn't count as special char)
+ */
 $.fn.checkSpecialChar = function() {
 	return !/[^A-Za-z0-9ÄÖÜäöüßéàè\s]+/.test(this.val())
 }
 
+/**
+ * Checks if a string contains whitespaces
+ */
 $.fn.checkWhitespace = function() {
 	return !/[\s]+/.test(this.val())
 }
 
+/**
+ * Checks if a string contains mutation
+ */
 $.fn.checkMutation = function() {
 	return !/[ÄÖÜäöüßéàè]+/.test(this.val())
 }
 
+/**
+ * Checks if a strings length is between two digits
+ */
 $.fn.checkLength = function(min, max) {
 	return this.val().length >= min && this.val().length <= max
 }
 
+/**
+ * Checks if a password is valid:
+ *  - only A-Za-z0-9#%&\/()?!$-_]+ used
+ *  - at least one digit or special char
+ *  - length between 6 and 50 chars
+ */
 $.fn.checkPassword = function() {
 	return !/[^A-Za-z0-9#%&\/()?!$-_]+/.test(this.val()) && /[0-9]{1}|[#%&\/()?!$-_]{1}/.test(this.val()) && this.checkLength(6, 50)
 }
 
+/**
+ * Checks if a time is valid
+ */
 $.fn.checkTime = function() {
 	return /^[\d]{1,2}:[\d]{2}$/.test(this.val()) && this.checkLength(4, 5)
 }
